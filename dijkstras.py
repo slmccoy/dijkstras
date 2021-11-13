6ee54f407c2a1354de70a4335fed7446c6b8203f
sarah <sarahlmccoy24@gmail.com>
2021-11-13 23:09:20 +0000

    Added Dijkstras algorithm

--- dijkstras.py
+++ dijkstras.py
@@ -11,6 +11,8 @@ file_data = np.genfromtxt(file,dtype = [
 #Split file data into lists (rather than tuples) inside a list - for indexing
 data = np.array([list(path) for path in file_data])
 
+total_weight = sum([int(weight) for weight in data[:,2]])
+
 nodes = set() # as we do not want duplicates
 nodes.update(data[:,0],data[:,1])
 size = len(nodes)
@@ -24,6 +26,47 @@ for path in data:
     end = path[1]
     weight = path[2]
 
-    network[start][end]=weight
+    network[start][end]=int(weight)
 
-print(network)
+'''
+Dijkstras
+'''
+
+start = 'A'
+end = 'F'
+
+unvisited = dict(zip(nodes,[total_weight]*size))
+unvisited[start] = 0
+
+solution = unvisited.copy()
+
+# max iterations is one per node
+for iteration in range(size):
+
+    # get minimum distance of unvisited to select next node
+    current = min(unvisited, key = unvisited.get)
+
+    # to avoid unnecessary loops
+    if current == end:
+        break
+
+    current_distance = solution[current]
+
+    # create list of connected nodes
+    connected = network[current]
+
+    for next in connected.keys():
+        if next not in unvisited.keys():
+            continue
+
+        weight = connected[next]
+
+        proposed_distance = current_distance + weight
+        # if the new distance is less than previous - update distance
+        if proposed_distance < solution[next]:
+            solution[next] = proposed_distance
+            unvisited[next] = proposed_distance
+
+    del unvisited[current]
+
+print(solution)
