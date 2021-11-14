10c0d835fd30e4417ad533bf27707b2b53ede973
sarah <sarahlmccoy24@gmail.com>
2021-11-14 00:15:51 +0000

    Add commandline run and functions

--- dijkstras.py
+++ dijkstras.py
@@ -1,89 +1,102 @@
 import numpy as np
+import sys
 
-file = 'dijkstras_example.dat'
+def extract_data(file, start,end):
+    '''
+    Extract data from .dat file and create dictionary
+    '''
 
-file_data = np.genfromtxt(file,dtype = [
-    ('start','<U10'),
-    ('end','<U10'),
-    ('weight','<i8')
-])
+    file_data = np.genfromtxt(file,dtype = [
+        ('start','<U10'),
+        ('end','<U10'),
+        ('weight','<i8')
+    ])
 
-#Split file data into lists (rather than tuples) inside a list - for indexing
-data = np.array([list(path) for path in file_data])
+    #Split file data into lists (rather than tuples) inside a list - for indexing
+    data = np.array([list(path) for path in file_data])
 
-total_weight = sum([int(weight) for weight in data[:,2]])
+    total_weight = sum([int(weight) for weight in data[:,2]])
 
-nodes = set() # as we do not want duplicates
-nodes.update(data[:,0],data[:,1])
-size = len(nodes)
+    nodes = set() # as we do not want duplicates
+    nodes.update(data[:,0],data[:,1])
+    size = len(nodes)
 
-# create nested dictionary
-network = dict(zip(nodes,[{} for _ in range(size)]))
+    # create nested dictionary
+    network = dict(zip(nodes,[{} for _ in range(size)]))
 
-# populate nested dictionary with weights
-for path in data:
-    start = path[0]
-    end = path[1]
-    weight = path[2]
+    # populate nested dictionary with weights
+    for path in data:
+        path_start = path[0]
+        path_end = path[1]
+        weight = path[2]
 
-    network[start][end]=int(weight)
+        network[path_start][path_end]=int(weight)
 
-'''
-Dijkstras
-'''
+    return network, total_weight
 
-start = 'A'
-end = 'F'
+def dijkstras(network, total_weight, start, end):
 
-unvisited = dict(zip(nodes,[total_weight]*size))
-unvisited[start] = 0
+    # recreated here to simplify function requirement
+    nodes = network.keys()
+    size = len(nodes)
 
-solution = unvisited.copy()
+    unvisited = dict(zip(nodes,[total_weight]*size))
+    unvisited[start] = 0
 
-previous = dict(zip(nodes,['']*size))
+    solution = unvisited.copy()
 
-# max iterations is one per node
-for iteration in range(size):
+    previous = dict(zip(nodes,['']*size))
 
-    # get minimum distance of unvisited to select next node
-    current = min(unvisited, key = unvisited.get)
+    # max iterations is one per node
+    for iteration in range(size):
 
-    # to avoid unnecessary loops
-    if current == end:
-        break
+        # get minimum distance of unvisited to select next node
+        current = min(unvisited, key = unvisited.get)
 
-    current_distance = solution[current]
+        # to avoid unnecessary loops
+        if current == end:
+            break
 
-    # create list of connected nodes
-    connected = network[current]
+        current_distance = solution[current]
 
-    for next in connected.keys():
-        if next not in unvisited.keys():
-            continue
+        # create list of connected nodes
+        connected = network[current]
 
-        weight = connected[next]
+        for next in connected.keys():
+            if next not in unvisited.keys():
+                continue
 
-        proposed_distance = current_distance + weight
-        # if the new distance is less than previous - update distance
-        if proposed_distance < solution[next]:
-            solution[next] = proposed_distance
-            unvisited[next] = proposed_distance
-            previous[next] = current
+            weight = connected[next]
 
-    del unvisited[current]
+            proposed_distance = current_distance + weight
+            # if the new distance is less than previous - update distance
+            if proposed_distance < solution[next]:
+                solution[next] = proposed_distance
+                unvisited[next] = proposed_distance
+                previous[next] = current
 
-path = []
+        del unvisited[current]
 
-# by using range iteration it limited iteration to number of visited nodes
-# current will start with end of iteration - should be end node
-for create_path in range(iteration):
-    path.append(current)
-    if current == start:
-        break
-    current = previous[current]
-    if current == '':
-        print('No Path Exists')
-        exit()
+    path = []
 
-path.reverse()
-print(path)
+    # by using range iteration it limited iteration to number of visited nodes
+    # current will start with end of iteration - should be end node
+    for create_path in range(iteration):
+        path.append(current)
+        if current == start:
+            break
+        current = previous[current]
+        if current == '':
+            print('No Path Exists')
+            exit()
+
+    path.reverse()
+    return path
+
+if __name__ == '__main__':
+
+    name, file, start, end = sys.argv
+    network, total_weight = extract_data(file, start, end)
+    path = dijkstras(network, total_weight ,start,end)
+    for node in path:
+        print(node)
